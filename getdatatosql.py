f7c92783652c3a9732e4f9561674e1977cc36301
Dision <964746175@qq.com>
2021-01-17 16:34:07 +0800

    面向对象化

--- getdatatosql.py
+++ getdatatosql.py
@@ -1,60 +1,73 @@
+# coding:utf-8
+
 import requests
 import pymysql
 from bs4 import BeautifulSoup
 
 
-def get_data():
-    datalist = []
-    city = 'sz'
-    city_url = 'http://%s.lianjia.com' % city
-    for page_num in range(1, 11):
-        url = "%s/zufang/pg%d/" % (city_url, page_num)
-        headers = {
+class GetData(object):
+    def __init__(self):
+        self.url = "http://%s.lianjia.com/zufang/pg%d"
+        self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/65.0.3325.181 Safari/537.36 '
         }
-        response = requests.request("GET", url, headers=headers)
-        htmlSoup = BeautifulSoup(response.text, "html.parser")
-        div = htmlSoup.find(attrs={"class": "content__list"})
-        if div is None:
-            continue
+
+    def parse_url(self, url):
+        response = requests.request("GET", url, headers=self.headers)
+        return response
+
+    def soup_url(self, response):
+        htmlsoup = BeautifulSoup(response.text, "html.parser")
+        return htmlsoup
+
+    def get_data(self, htmlsoup, url, datalist):
+        div = htmlsoup.find(attrs={"class": "content__list"})
         div_list = div.find_all("div", {'class': 'content__list--item'})
-        if div_list is None:
-            continue
         for info in div_list:
             house_url = info.find('a')['href']
             address = info.find('a', {'class': 'content__list--item--aside'})['title']
             money = info.find('span', {'class': 'content__list--item-price'}).text
-            data = {'url': '%s%s' % (city_url, house_url), 'address': address, 'money': money}
+            data = {'url': '%s%s' % (url, house_url), 'address': address, 'money': money}
             datalist.append(data)
-    return datalist
+        return datalist
 
+    def save_data(self, datalist):
+        db = pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             password='mysql',
+                             db='renthouse')
+        cursor = db.cursor()
+        for j in range(len(datalist)):
+            datarow = datalist[j]
+            addr = datarow['address']
+            url = datarow['url']
+            money = datarow['money']
+            sql = "INSERT INTO houses(url,address,money)VALUES('%s','%s','%s')" % (url, addr, money)
+            try:
+                cursor.execute(sql)
+                db.commit()
+            except Exception as e:
+                print('数据写入失败', e)
+                db.rollback()
+                db.close()
 
-def into_mysql(datalist):
-    db = pymysql.connect(host='127.0.0.1',
-                         user='root',
-                         password='mysql',
-                         db='renthouse')
-    cursor = db.cursor()
-    for j in range(len(datalist)):
-        datarow = datalist[j]
-        addr = datarow['address']
-        hurl = datarow['url']
-        money = datarow['money']
-        sql = "INSERT INTO houses(url,address,money)VALUES('%s','%s','%s')" % (hurl, addr, money)
-        try:
-            cursor.execute(sql)
-            db.commit()
-        except Exception as e:
-            print('数据写入失败', e)
-            db.rollback()
-            db.close()
-
-
-def main():
-    datalist = get_data()
-    into_mysql(datalist)
+    def run(self):
+        city = 'gz'  # 爬取的城市，请对照链家的城市简写
+        datalist = []  # 存放数据
+        # 1.获取url
+        for page_num in range(1, 11):
+            url = self.url % (city, page_num)
+            # 2.发送请求获取响应
+            response = self.parse_url(url)
+            # 3.解析数据
+            htmlsoup = self.soup_url(response)
+            # 4.提取数据
+            datalist = self.get_data(htmlsoup, url, datalist)
+        # 5.保存数据
+        self.save_data(datalist)
 
 
 if __name__ == '__main__':
-    main()
+    getdatatosql = GetData()
+    getdatatosql.run()
